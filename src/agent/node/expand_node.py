bef34aa3c96a2d0ff476254b94a7b93ba7f1a364
baitiaojun <2731070169@qq.com>
2026-04-05 22:36:36 +0800

    expand_node.py去掉注释代码

--- src/agent/node/expand_node.py
+++ src/agent/node/expand_node.py
@@ -46,40 +46,6 @@ async def expand_node(state: OverallState, runtime: Runtime[EnvContext]):
         dialect=dialect,
     )
 
-    # # 给过滤后的列表扩展维度表主键和事实表外键，用于后续连表SQL生成
-    # for table_info in filter_table_info_list:
-    #     table_role = table_info.role
-    #     column_roles = [column.role for column in table_info.columns]
-    #     # 如果是维度表且没有主键字段，那么就加上主键
-    #     if 'dim' in table_role and 'primary_key' not in column_roles:
-    #         primary_key = await meta_repository.get_column_primary_key_by_table_name(table_info.name)
-    #         if primary_key:
-    #             table_info.columns.append(TableColumnState(
-    #                 name=primary_key.name,
-    #                 type=primary_key.type,
-    #                 role=primary_key.role,
-    #                 description=primary_key.description,
-    #                 examples=primary_key.examples,
-    #                 alias=primary_key.alias
-    #             ))
-    #     # 如果是实时表，就加上外键
-    #     elif 'fact' in table_role:
-    #         foreign_keys = await meta_repository.get_columns_by_role_and_table_role('foreign_key', 'fact')
-    #         column_names = [column.name for column in table_info.columns]
-    #         # 遍历每一个外键，仅添加没有的外键
-    #         for foreign_key in foreign_keys:
-    #             if foreign_key.name not in column_names:
-    #                 table_info.columns.append(
-    #                     TableColumnState(
-    #                         name=foreign_key.name,
-    #                         type=foreign_key.type,
-    #                         role=foreign_key.role,
-    #                         description=foreign_key.description,
-    #                         examples=foreign_key.examples,
-    #                         alias=foreign_key.alias
-    #                     )
-    #                 )
-
     logger.info(
         f"扩展后的当前时间: {current_time}, "
         f"当前季度: {current_quarter}, "
